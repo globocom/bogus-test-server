a4857c5036448ba5ced1c41207eb7da83dff4cb5
Flavia Missi <flaviamissi@gmail.com>
2014-10-23 16:37:42 -0200

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='bogus',
+      version='0.1.0',
+      description='A simple bogus server to use in tests',
+      author='Globo.com',
+      author_email='flavia.missi@corp.globo.com',
+      url='https://github.com/globocom/bogus-test-server#bogus-test-server',
+     )
