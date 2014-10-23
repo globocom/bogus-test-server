25b32a9b9b200a096933671013a03adcb09fc6fe
Flavia Missi <flaviamissi@gmail.com>
2014-10-23 15:02:09 -0200

    first version of request parsing and handling
    handling still not finished, it doesn't repass parameters to the handler
    function as it should and doesn't accepts registration of paths with
    regex or other parameters passing solutions.

--- bogus/server.py
+++ bogus/server.py
@@ -3,11 +3,80 @@ import SocketServer
 from threading import Thread
 
 
-class BogusHandler(SocketServer.BaseRequestHandler):
+class BogusHandler(SocketServer.StreamRequestHandler):
+    """
+    Custom handler to use in TCPServer.
+    This handler has hability to map requests to registered handler callers.
+    It doesn't check anything about the request, unless you don't send a method or a
+    status code, which we need to map the handler to the request being made.
+    """
 
     def handle(self):
-        self.request.sendall("HTTP/1.1 200")
+        """
+        Handles requests by parsing them and finding if there's any handlers registered to
+        handle them, if it doesn't find any handlers and promiscuous is set to True, it
+        responds with an empty body and a 200 status code, if promiscuous is set to False
+        it returns an empty body and a 404 status code.
+        """
+        self.raw_requestline = self.rfile.readline(65537)
+        self.parse_request()
 
+        handler = self.find_handler()
+        if handler:
+            body, status = handler()
+            self.request.sendall("HTTP/1.1 {0} OK\r\n{1}".format(status, body))
+            return
+
+        self.request.sendall("HTTP/1.1 200 OK")
+
+    def find_handler(self):
+        if not hasattr(self, "handlers"):
+            return False
+
+        if not self.method in self.handlers.keys():
+            return False
+        else:
+            for path, handler in self.handlers[self.method]:
+                if path == self.path:
+                    return handler
+                    break
+
+    def parse_request(self):
+        """
+        Parses the request first line to get the status and method.
+        Saves it on instances variables.
+        """
+        reqline = self.raw_requestline.rstrip('\r\n')
+        self.requestline = reqline
+
+        words = reqline.split()
+        if len(words) == 3:
+            method, path, _ = words
+        elif len(words) == 2:
+            method, path = words
+        else:
+            self.send_error(400, "Bad request syntax (%r)" % requestline)
+            return
+
+        self.method = method
+        self.path = path
+
+
+    @classmethod
+    def register_handler(cls, handler, method="GET"):
+        """
+        Register a handler within a given method, the default method is get.
+        The data structure used to store the handler is a dictionary of arrays, each array
+        entry contains the handler tuple (path, callable). It looks like this:
+            {"GET": ("/profile", profileHandler), ("/settings", settingsHandler)}
+        The registered handler should return 2 values, the response body and status code.
+        """
+        if not hasattr(cls, "handlers"):
+            cls.handlers = {method: [handler]}
+        elif method in cls.handlers.keys():
+            cls.handlers[method].append(handler)
+        else:
+            cls.handlers[method] = [handler]
 
 
 class Bogus(object):
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,6 +1,7 @@
 import unittest
 import requests
-from bogus.server import Bogus
+from bogus.server import Bogus, BogusHandler
+from mock import Mock
 
 class BogusTest(unittest.TestCase):
 
@@ -17,6 +18,7 @@ class BogusTest(unittest.TestCase):
         url = b.serve()
         self.assertEqual(url, b.url)
 
+    # intermitent?
     def test_serve_should_start_server_and_respond_to_any_request(self):
         b = Bogus()
         url = b.serve()
@@ -25,3 +27,34 @@ class BogusTest(unittest.TestCase):
         self.assertEqual(response.content, "")
         response = requests.get("{}/something-else".format(url))
         self.assertEqual(response.status_code, 200)
+
+
+class BogusHandlerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.request_mock = Mock()
+        # mocks the request line, see http://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html#sec5
+        self.request_mock.makefile.return_value.readline.return_value = "GET /profile HTTP/1.1"
+
+    def test_should_have_a_handle_method(self):
+        bh = BogusHandler(self.request_mock, "client_address", "server")
+        self.assertTrue(hasattr(bh, "handle"))
+
+    def test_should_register_handlers(self):
+        handler = ("/profile", lambda: ("Profile", 200))
+        BogusHandler.register_handler(handler)
+        self.assertIn(handler, BogusHandler.handlers["GET"])
+        handler = ("/register", lambda: ("Register", 200))
+        BogusHandler.register_handler(handler)
+        self.assertIn(handler, BogusHandler.handlers["GET"])
+
+        handler = ("/register", lambda: ("Register", 200))
+        BogusHandler.register_handler(handler, method="POST")
+        self.assertIn(handler, BogusHandler.handlers["POST"])
+
+    def test_should_register_handler_and_respond_to_request_for_that_handler(self):
+        BogusHandler.register_handler(("/profile", lambda: ("Profile", 200)))
+        bh = BogusHandler(self.request_mock, "client_address", "server")
+        bh.handle()
+        expected_response = "HTTP/1.1 200 OK\r\nProfile"
+        self.request_mock.sendall.assert_called_with(expected_response)
